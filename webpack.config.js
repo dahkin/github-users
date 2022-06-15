d5c4105588f34ae8cbd76b47bb652c70c85dd16c
Daria Zakharova <daria.zaharova@emergn.com>
2022-06-15 23:13:56 +0300

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
-    publicPath: '/',
+    publicPath: 'github-users/',
   },
   module: {
     rules: [
